9c2a63ff18d812599ca5b197dec490eaa4ee91fb
Turing777 <1246683848@qq.com>
2018-06-30 14:34:21 +0800

    commit

---
+++ ymjhuahua-parent/ymjhuahua-base/src/main/java/com/avalon/base/YmjhuahuaBaseApplication.java
@@ -0,0 +1,12 @@
+package com.avalon.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmjhuahuaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmjhuahuaBaseApplication.class, args);
+    }
+}
---
+++ ymjhuahua-parent/ymjhuahua-service/src/main/java/com/avalon/ymjhuahua/config/DruidDBConfig.java
@@ -0,0 +1,53 @@
+package com.avalon.ymjhuahua.config;
+
+import com.avalon.ymjhuahua.TestEntity;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DruidDBConfig {
+
+    @Autowired
+    private TestEntity testEntity;
+
+    @Bean(name = "dataSource")
+    @Qualifier(value = "dataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        System.out.println(testEntity.getName());
+        DataSource dataSource = DataSourceBuilder.create().type(com.alibaba.druid.pool.DruidDataSource.class).build();
+        return  dataSource;
+//        return DataSourceBuilder.create().type(com.alibaba.druid.pool.DruidDataSource.class).build();
+    }
+
+    @Bean(name = "mainSqlSessionFactory")
+    public SqlSessionFactory testSqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        //        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/message/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean(name = "mainTransactionManager")
+    public DataSourceTransactionManager testTransactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "mainSqlSessionTemplate")
+    public SqlSessionTemplate testSqlSessionTemplate(
+            @Qualifier("mainSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
