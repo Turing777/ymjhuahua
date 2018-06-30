22094d34d1f8e9f9e9155a01aeebfa3da3c6cda0
Turing777 <1246683848@qq.com>
2018-06-30 14:50:58 +0800

    commit

---
+++ ymjhuahua-parent/ymjhuahua-base/src/main/java/com/avalon/base/entity/TestEntity.java
@@ -0,0 +1,19 @@
+package com.avalon.base.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="spring.datasource")
+public class TestEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
