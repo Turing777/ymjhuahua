d2341232f21d5972a5690951479f6e42a631c641
Truing777 <1246683848@qq.com>
2018-07-02 15:45:46 +0800

    commit

--- ymjhuahua-parent/ymjhuahua-config/src/main/java/com/avalon/ymjhuahua/YmjhuahuaConfigApplication.java
+++ ymjhuahua-parent/ymjhuahua-config/src/main/java/com/avalon/ymjhuahua/YmjhuahuaConfigApplication.java
@@ -1,8 +1,16 @@
 package com.avalon.ymjhuahua;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableConfigServer
+@RefreshScope
 @SpringBootApplication
 public class YmjhuahuaConfigApplication {
 
