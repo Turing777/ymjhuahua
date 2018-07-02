88e356474356a082e72e0f973db6fd3543984cd6
Truing777 <1246683848@qq.com>
2018-07-02 14:59:29 +0800

    commit

---
+++ ymjhuahua-parent/ymjhuahua-config/src/main/java/com/avalon/ymjhuahua/YmjhuahuaConfigApplication.java
@@ -0,0 +1,12 @@
+package com.avalon.ymjhuahua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmjhuahuaConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmjhuahuaConfigApplication.class, args);
+    }
+}
